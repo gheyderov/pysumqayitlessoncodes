4d25f9d09368c74359a0ee28bbe8e933518c31dc
Gunduz <gunduz@Gunduzs-MacBook-Air.local>
2023-05-04 19:50:26 +0400

    inheritance

--- app.py
+++ app.py
@@ -2,26 +2,45 @@ from flask import Flask, render_template
 
 app = Flask(__name__,)
 
-students = {
-    'user_1' : {
-        'first_name' : 'John',
-        'last_name' : 'Doe',
-        'age' : 20
+items = {
+    1: {
+        'id' : 1,
+        'title': 'Product #1',
+        'description': 'Some Description #1',
+        'image': 'shoes1.jpeg',
+        'price': 100,
+        'quantity': 10,
+        'colors' : ['red', 'blue'],
+        'sizes' : ['S', 'M']
     },
-    'user_2' : {
-        'first_name' : 'Kelly',
-        'last_name' : 'Doe',
-        'age' : 22
+    2: {
+        'id' : 2,
+        'title': 'Product #2',
+        'description': 'Some Description #2',
+        'image': 'shoes2.jpeg',
+        'price': 200,
+        'quantity': 20,
+        'colors' : ['yellow', 'blue'],
+        'sizes' : ['XS', 'M']
     },
-    'user_3' : {
-        'first_name' : 'Smith',
-        'last_name' : 'Doe',
-        'age' : 15
-    },
+    3: {
+        'id' : 3,
+        'title': 'Product #3',
+        'description': 'Some Description #3',
+        'image': 'shoes3.jpeg',
+        'price': 300,
+        'quantity': 30,
+        'colors' : ['green', 'blue'],
+        'sizes' : ['XL', 'M']
+    }
 }
 
-colors = ['red', 'blue', 'green']
 
 @app.route("/")
-def hello_world():
-    return render_template('index.html', users = students, colors = colors)
+def home():
+    return render_template('index.html', items = items)
+
+
+@app.route("/product/<int:id>/")
+def productDetail(id):
+    return render_template('product.html', item = items[id])
