a654743a2dc4660150d90982b11bc651783ad116
Gunduz <gunduz@Gunduzs-MacBook-Air.local>
2023-07-24 20:07:34 +0400

    django session

--- django-codes/stories/views.py
+++ django-codes/stories/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, HttpResponse
 from stories.models import Recipe, Category, Tag
+from django.contrib import messages
 
 # Create your views here.
 
 def recipes(request):
+    print('Liked posts: ', request.session.get('liked_posts'))
     recipes = Recipe.objects.all()
     categories = Category.objects.all()
     context = {
@@ -21,3 +23,14 @@ def recipe_detail(request, pk):
         'tags' : tags
     }
     return render(request, 'single.html', context)
+
+
+# def like_post(request, pk):
+#     request.session['liked_posts'] = request.session.get('liked_posts', ' ') + str(pk) + ' '
+#     messages.add_message(request, messages.SUCCESS, "Liked!")
+#     return render(request, 'recipes.html')
+
+def like_post(request, pk):
+    response = HttpResponse('like')
+    response.set_cookie('liked_posts', request.COOKIES.get('liked_posts', ' ') + str(pk) + ' ')
+    return response
